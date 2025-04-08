9dbcb71e2ca58b717025d2785be4953ef14af302
xin <s415003.ykvs@gmail.com>
2025-04-08 10:12:21 +0800

    更新bootstrap

--- resources/static/js/appointment_update.js
+++ resources/static/js/appointment_update.js
@@ -1,4 +1,42 @@
 $(document).ready(function () {
+	let today = new Date().toISOString().split("T")[0];
+	   $('#datepicker').datepicker({
+	       format: 'yyyy-mm-dd',
+	       autoclose: true,
+	       todayHighlight: true,
+		   startDate: today
+	   }).on("changeDate", function (e) {
+	       var selectedDate = $('#appointmentDate').val();
+	       console.log("選擇的日期是: " + selectedDate);
+	       if (!selectedDate) {
+	           alert("請選擇一個日期");
+	           return;
+	       }
+
+	       $.ajax({
+	           url: "/appointment/queryBookingTime",
+	           type: "GET",
+	           data: { appointmentDate: selectedDate },
+	           dataType: "json",
+	           success: function (response) {
+	               console.log(response);
+	               var bookedTimeslots = response.bookedTimeslots || [];
+	               var allTimeslots = ["10:00-12:00", "12:00-14:00", "14:00-16:00", "16:00-18:00", "18:00-20:00"];
+	               var timeslotSelect = $("#appointmentTimeslot");
+	               timeslotSelect.empty();
+	               allTimeslots.forEach(function (slot) {
+	                   var option = $("<option></option>").val(slot).text(slot);
+	                   if (bookedTimeslots.includes(slot)) {
+	                       option.prop("disabled", true);
+	                   }
+	                   timeslotSelect.append(option);
+	               });
+	           },
+	           error: function (xhr, status, error) {
+	               console.error("無法獲取預約資料: " + error);
+	           }
+	       });
+	   });
 
     $('#updateButton').click(function (e) {
         e.preventDefault();
